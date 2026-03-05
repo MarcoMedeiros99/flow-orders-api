5bb355be2d1d8cb7adfca1ef886d595066320673
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-05 20:32:13 -0300

    Project created

---
+++ src/main/java/io/github/marcoMedeiros/flow_orders_backend/FlowOrdersBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcoMedeiros.flow_orders_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowOrdersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowOrdersBackendApplication.class, args);
+	}
+
+}
