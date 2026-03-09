2f17fc202a591133d47b756811b174ed49bbf852
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-09 20:44:30 -0300

    H2 database, test profile, JPA

--- src/main/java/io/flow_orders_backend/FlowOrdersBackendApplication.java
+++ src/main/java/io/flow_orders_backend/FlowOrdersBackendApplication.java
@@ -1,4 +1,4 @@
-package io.github.marcoMedeiros.flow_orders_backend;
+package io.flow_orders_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/flow_orders_backend/resource/UserResource.java
+++ src/main/java/io/flow_orders_backend/resource/UserResource.java
@@ -1,11 +1,11 @@
-package io.github.marcoMedeiros.flow_orders_backend.resource;
+package io.flow_orders_backend.resource;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.marcoMedeiros.flow_orders_backend.entities.User;
+import io.flow_orders_backend.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
