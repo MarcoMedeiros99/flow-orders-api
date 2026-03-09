12103cba8bd7679e3366e176368284c3ad24580e
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-09 20:44:30 -0300

    H2 database, test profile, JPA

--- src/main/java/io/flow_orders_backend/entities/User.java
+++ src/main/java/io/flow_orders_backend/entities/User.java
@@ -1,11 +1,21 @@
-package io.github.marcoMedeiros.flow_orders_backend.entities;
+package io.flow_orders_backend.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
