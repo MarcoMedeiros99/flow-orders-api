7ad55c949dab8b7ce4b4ee8cfa88aa5228857446
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-11 18:51:41 -0300

    Service layer, component registration

--- src/main/java/io/flow_orders_backend/FlowOrdersBackendApplication.java
+++ src/main/java/io/flow_orders_backend/FlowOrdersBackendApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlowOrdersBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowOrdersBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(FlowOrdersBackendApplication.class, args);
+    }
 
 }
--- src/main/java/io/flow_orders_backend/config/TestConfig.java
+++ src/main/java/io/flow_orders_backend/config/TestConfig.java
@@ -19,8 +19,8 @@ public class TestConfig implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(null, "Thomas Wayne", "thomas@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Bruce Wayne", "Bruce@gmail.com", "977777777", "123456");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
