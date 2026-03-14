9eaeb2f8b33c509ae87936305f2d7a1c623c8276
MarcoMedeiros99 <marcomed07@gmail.com>
2026-03-14 00:00:18 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/io/flow_orders_backend/entities/Order.java
+++ src/main/java/io/flow_orders_backend/entities/Order.java
@@ -3,16 +3,12 @@ package io.flow_orders_backend.entities;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.flow_orders_backend.entities.enums.OrderStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_order")
@@ -33,6 +29,9 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Order() {
     }
 
@@ -44,24 +43,43 @@ public class Order implements Serializable {
         this.client = client;
     }
 
-    public Long getId() { return id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public Instant getMoment() { return moment; }
-
-    public void setMoment(Instant moment) { this.moment = moment; }
-
-    public OrderStatus getOrderStatus() { return OrderStatus.valueOf(orderStatus); }
-
-    public void setOrderStatus(OrderStatus orderStatus) {
-        if (orderStatus != null){
-        this.orderStatus = orderStatus.getCode();}
+    public Long getId() {
+        return id;
     }
 
-    public User getClient() { return client; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public void setClient(User client) { this.client = client; }
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Set<OrderItem> getItems(){
+        return items;
+    }
 
     @Override
     public int hashCode() {
@@ -83,8 +101,7 @@ public class Order implements Serializable {
         if (id == null) {
             if (other.id != null)
                 return false;
-        }
-        else if (!id.equals(other.id))
+        } else if (!id.equals(other.id))
             return false;
         return true;
     }
